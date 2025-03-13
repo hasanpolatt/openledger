38e30b5ad54c31004332e575fefb705382c45df0
hasanpolatt <thatsnotpublic@gmail.com>
2025-03-13 15:28:53 +0300

    refactor: add docstrings

--- travelai/src/main.py
+++ travelai/src/main.py
@@ -22,5 +22,5 @@ if __name__ == "__main__":
         "travelai.src.main:app",
         host="0.0.0.0",
         port=8000,
-        reload=True,  # Development modunda hot-reload aktif
+        reload=True
     )
